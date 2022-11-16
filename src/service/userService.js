275dfeb7f5bd6a78699650723cf654f20eb89327
Dao Duong <dao.duong@saritasa.com>
2022-11-16 16:11:30 +0700

    add user endpoint

---
+++ src/service/userService.js
@@ -0,0 +1,22 @@
+import User from '../model/user.js'
+import bcrypt from 'bcryptjs'
+
+const UserService = {}
+
+UserService.getUserById = async (id) => {
+  return await User.findOne({ _id: id }).select('-password')
+}
+UserService.update = async (id, updateData) => {
+  if (updateData.password) {
+    updateData.password = await bcrypt.hash(updateData.password, 10)
+    await User.findByIdAndUpdate(id, updateData)
+  } else {
+    await User.findByIdAndUpdate(id, updateData)
+  }
+  return await User.findOne({ _id: id })
+}
+UserService.delete = async (id) => {
+  await User.findByIdAndRemove(id)
+  return await User.find()
+}
+export default UserService
